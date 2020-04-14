9f16d54266324ce5b2162117210da19f1b7f5d34
hidri <agthidri2400@gmail.com>
2020-04-14 17:57:40 +0300

    this commi will add rthe javascript

--- js/script.js
+++ js/script.js
@@ -1,19 +1,48 @@
 
 
+var sideOne, sideTwo, sideThree;
 
-var a , b ,c;
 
-function getdat(){
-    a = document.getElementById('a').value;
-    b = document.getElementById('b').value;
-    c = document.getElementById('c').value;
 
+function getdata(){
+  
+    this.sideOne= document.getElementById("sideOne").value;
+    this.sideTwo=document.getElementById('sideTwo').value;
+    this.sideThree=document.getElementById("sideThree").value;
+    this.validateData();
+}
+function validateData(){
+    if(this.sideOne <= 0 || this.sideTwo <= 0){
+        alert("Invalid Triangle");
+        return;
+    }
+    if(this.sideThree <=0 || this.sideOne <=0 ){
+        alert("Invalid Triangle");
+        return;
+    }
+    if  (this.sideOne +  this.sideTwo <= this.sideThree || this.sideOne + this.sideThree <= this.sideTwo || this.sideTwo + this.sideThree <= this.sideOne){
+        alert("this sides can't construct trianlge")
+        return;
+    }
+
+    this.finalResult();
+}
+function finalResult(){
+    // var result = parseInt(calculateEriName());
+   
+        if  (this.sideOne === this.sideTwo && sideTwo === sideThree){
+            alert("Triangle is equilateral triangle")
+    }
+    
+    else if (this.sideOne === this.sideTwo && this.sideTwo !== this.sideThree){
+        alert("Triangle is isosceles triangle" )
+    }
+    else{
+        alert("Triangle is scalne Triangle")
+    }
 }
 
-if (a === b || a === c) {
-    alert('This triangle is isoscles trainlge');
-}
-else if (a === b || a === c){
-    alert('this traingle is equalatrial');
-}
+// function calculateEriName(){
+//     return (((this.century / 4) - 2 * this.century - 1) + ((5 * this.year / 4)) + ((26 * (this.month + 1) / 10)) + this.day) % 7;
+// }
 
