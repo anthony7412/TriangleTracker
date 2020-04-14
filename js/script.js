b4c11a9441419939e96ca91b53e05b71e421d0a1
hidri <agthidri2400@gmail.com>
2020-04-14 21:40:48 +0300

    this commit will add the corected the mistake in js

--- js/script.js
+++ js/script.js
@@ -20,29 +20,26 @@ function validateData(){
         alert("Invalid Triangle");
         return;
     }
-    if  (this.sideOne +  this.sideTwo <= this.sideThree || this.sideOne + this.sideThree <= this.sideTwo || this.sideTwo + this.sideThree <= this.sideOne){
-        alert("this sides can't construct trianlge")
-        return;
-    }
-
+  
+         
     this.finalResult();
 }
 function finalResult(){
-    // var result = parseInt(calculateEriName());
    
-        if  (this.sideOne === this.sideTwo && sideTwo === sideThree){
+   
+    if  (this.sideOne === this.sideTwo && sideTwo === sideThree){
             alert("Triangle is equilateral triangle")
     }
     
     else if (this.sideOne === this.sideTwo && this.sideTwo !== this.sideThree){
         alert("Triangle is isosceles triangle" )
     }
+    else if((this.sideOne + this.sideTwo) <= this.sideThree|| (this.sideOne + this.sideThree) <= this.sideTwo || (this.sideTwo + this.sideThree) <= this.sideOne) {
+        alert("Not a definite triangle");
+        }
     else{
         alert("Triangle is scalne Triangle")
     }
 }
 
-// function calculateEriName(){
-//     return (((this.century / 4) - 2 * this.century - 1) + ((5 * this.year / 4)) + ((26 * (this.month + 1) / 10)) + this.day) % 7;
-// }
 
