cdac1cf2716833b5d01c2958113fe856fa31d3e9
hidri <agthidri2400@gmail.com>
2020-04-14 13:30:00 +0300

    This commt wil add the index.html

---
+++ js/script.js
@@ -0,0 +1,19 @@
+
+
+
+var a , b ,c;
+
+function getdat(){
+    a = document.getElementById('a').value;
+    b = document.getElementById('b').value;
+    c = document.getElementById('c').value;
+
+}
+
+if (a === b || a === c) {
+    alert('This triangle is isoscles trainlge');
+}
+else if (a === b || a === c){
+    alert('this traingle is equalatrial');
+}
+
